41d8491f375720d2de8d15e8b47c92d6cdbe1751
DaphniG <114948275+DaphniG@users.noreply.github.com>
2023-05-05 21:50:18 -0700

    Update queries.py

--- queries.py
+++ queries.py
@@ -4,16 +4,6 @@ import json
 with open('pokemon.json', 'r') as file:
     data = json.load(file)
 
-# Example query: get all records where "type" is "water"
-#pokemon = [record for record in data if record['name'] == 'Pikachu']
-# pikachus = [pokemon for pokemon in data if pokemon['name'] == 'Pikachu']
-# print(pikachus)
-#pokemon2 = [record for record in data if record['abilties'] == 'Pikachu']
-#print(pokemon)
-# Example query: get the names of all grass-type Pokemon
-#grass_pokemon_names = [record['name'] for record in data if 'grass' in record['typ
-# es']]
-#print(data)
 poke =[]
 for record in data:
     #print(record)
@@ -39,6 +29,5 @@ for record in data:
      if record['attack'] > 150:
             poke3.append(record)
 print(poke3)
-#overgrow_pokemon = [pokemon for pokemon in data['pokemon'] if 'Overgrow' in pokemon['abilities']]
   
    
